cce93b10006ca89158325b437092642cd5c99b39
王很水 <jieyaren@gmail.com>
2019-05-17 17:34:08 +0800

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,6 +2,6 @@
   const match = /^(?:.*\/)?([a-f0-9]+)$/.exec(window.location.href);
   const ID = match ? match[1] : null;
   if (ID) {
-    window.location.href = 'https://yi-jiayu.github.io/essence/#' + ID;
+    window.location.href = 'https://wanghenshui.github.io/gist-viewer/#' + ID;
   }
 })();
